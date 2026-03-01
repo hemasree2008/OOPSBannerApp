6862c78dfb8e98ca64d8cffa0f79cf3530d9d23f
hemasree2008 <hn9341@srmist.edu.in>
2026-03-01 23:16:05 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,20 +1,25 @@
-public class OOPSBannerApp {
-
- feature/UC2-BannerPrint
-public static void main(String[] args) {
-
-    System.out.println("  *****   *****   ******   *****  ");
-    System.out.println(" *     * *     *  *     *  *      ");
-    System.out.println(" *     * *     *  *     *  *      ");
-    System.out.println(" *     * *     *  ******   *****  ");
-    System.out.println(" *     * *     *  *            *  ");
-    System.out.println(" *     * *     *  *            *  ");
-    System.out.println("  *****   *****   *        *****  ");
+public class stringjoin{
 
     public static void main(String[] args) {
-
-        System.out.println("OOPS");
-
+        renderBanner();
     }
- main
+
+    public static void renderBanner() {
+
+        String line1 = String.join(" ", "*****",   "*****",  "****",  "*****");
+
+        String line2 = String.join(" ", "*   *",   "*   *",  "*  *",  "*    ");
+
+        String line3 = String.join(" ", "*   *",   "*   *",  "****",  "*****");
+
+        String line4 = String.join(" ", "*   *",   "*   *",  "*   ",  "    *");
+
+        String line5 = String.join(" ", "*****",   "*****",  "*   ",  "*****");
+
+        System.out.println(line1);
+        System.out.println(line2);
+        System.out.println(line3);
+        System.out.println(line4);
+        System.out.println(line5);
+    }
 }
